98e6728cd71b5a1af28e5501162b7e822a5c4b4d
W-Mai <1341398182@qq.com>
2023-04-20 01:40:23 +0800

    😁(Heap): support addr align, get_heap_top features
    fix warnings

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,8 @@ mod utils;
 
 // use core::arch::wasm32;
 use core::alloc::{GlobalAlloc, Layout};
-use core::cmp::{max, min};
 
-const HEAP_SIZE: usize = 8 * 1024 * 1024; // 1 MB
+const HEAP_SIZE: usize = 1024; // 1 KB
 
 static mut HEAP: [u8; HEAP_SIZE] = [0; HEAP_SIZE];
 
@@ -17,6 +16,7 @@ static mut HEAP_TOP: usize = 0;
 
 pub struct Heap;
 
+#[allow(dead_code)]
 unsafe impl GlobalAlloc for Heap {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         let size = layout.size();
@@ -24,15 +24,26 @@ unsafe impl GlobalAlloc for Heap {
 
         if HEAP_TOP < HEAP_SIZE {
             let ptr = HEAP.as_mut_ptr().add(HEAP_TOP);
-            HEAP_TOP += size;
-            return ptr;
+            let aligned_size = ptr.align_offset(align) + size; // get aligned size offset
+            if HEAP_TOP + aligned_size <= HEAP_SIZE {
+                HEAP_TOP += aligned_size;
+                return ptr;
+            }
         }
+
         core::ptr::null_mut()
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-        if ptr.is_null() {
-            return;
-        }
+        if ptr.is_null() {}
+    }
+}
+
+impl Heap {
+    /// # Safety
+    ///
+    /// This function is unsafe because it is not thread-safe.
+    pub fn get_heap_top() -> usize {
+        unsafe { HEAP_TOP }
     }
 }
